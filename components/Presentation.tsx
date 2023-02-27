d13262062832b970824a11da2e0c6ee5b807cb98
Pavel Štěrba <email@pavelsterba.com>
2023-02-27 09:36:51 +0100

    Odebrán x-scrollbar

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -6,7 +6,7 @@ interface PresentationProps {
 
 const Presentation = ({ children }: PresentationProps) => {
   return (
-    <main className='bg-pattern w-screen h-screen flex flex-row py-20 px-32 gap-10 snap-x snap-mandatory overflow-x-auto'>
+    <main className='bg-pattern w-screen h-screen flex flex-row py-20 px-32 gap-10 snap-x snap-mandatory overflow-hidden'>
       {children}
     </main>
   );
